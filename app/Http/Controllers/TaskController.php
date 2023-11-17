cb4b82ed9f5ca532a35c2ec8a4a87b59c9cea275
Marat Zargarov <maratzargarov@mail.ru>
2023-11-17 18:42:38 +0400

    Добавление шаблонов + методы в TaskController (добавление задачи, отображение всех задач)

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -7,6 +7,38 @@ use App\Http\Controllers\Controller;
 class TaskController extends Controller
 {
     /**
+     * Create a new task.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request, [
+            'name' => 'required|max:255',
+        ]);
+
+        $request->user()->tasks()->create([
+            'name' => $request->name,
+        ]);
+
+        return redirect('/tasks');
+    }
+    /**
+     * Display a list of all of the user's task.
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function index(Request $request)
+    {
+        $tasks = $request->user()->tasks()->get();
+
+        return view('tasks.index', [
+            'tasks' => $tasks,
+        ]);
+    }
+    /**
      * Create a new controller instance.
      *
      * @return void
