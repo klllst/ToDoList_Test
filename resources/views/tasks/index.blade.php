bcd47a110f5a9ead4f090ad96d2e8109a552207c
Marat Zargarov <maratzargarov@mail.ru>
2023-11-18 11:59:42 +0400

    Изменения в шаблонах

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,77 +2,70 @@
 
 @section('content')
     <div class="container">
-        <div class="col-sm-offset-2 col-sm-8">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    New Task
-                </div>
-
-                <div class="panel-body">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
                     <!-- Display Validation Errors -->
                     @include('common.errors')
+                    <div class="card-header">{{__('New Task')}}</div>
+                    <div class="card-body">
+                        <!-- New Task Form -->
+                        <form action="{{ url('task') }}" method="POST" class="form-horizontal">
+                            {{ csrf_field() }}
+                            <!-- Task Name -->
+                            <div class="form-group">
+                                <label for="task-name" class="col-sm-3 control-label">Task</label>
 
-                    <!-- New Task Form -->
-                    <form action="{{ url('task') }}" method="POST" class="form-horizontal">
-                        {{ csrf_field() }}
-
-                        <!-- Task Name -->
-                        <div class="form-group">
-                            <label for="task-name" class="col-sm-3 control-label">Task</label>
-
-                            <div class="col-sm-6">
-                                <input type="text" name="name" id="task-name" class="form-control" value="{{ old('task') }}">
+                                <div class="col-sm-6">
+                                    <input type="text" name="name" id="task-name" class="form-control" value="{{ old('task') }}">
+                                </div>
                             </div>
-                        </div>
 
-                        <!-- Add Task Button -->
-                        <div class="form-group">
-                            <div class="col-sm-offset-3 col-sm-6">
-                                <button type="submit" class="btn btn-success">
-                                    <i class="fa-btn"></i>Add Task
-                                </button>
+                            <!-- Add Task Button -->
+                            <div class="form-group mt-3">
+                                <div class="col-sm-offset-3 col-sm-6">
+                                    <button type="submit" class="btn btn-success">
+                                        <i class="fa-btn"></i>Add Task
+                                    </button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    </div>
                 </div>
+                <!-- Current Tasks -->
+                @if (count($tasks) > 0)
+                    <div class="card mt-4">
+                        <div class="card-header">{{__('Tasks')}}</div>
+
+                        <div class="card-body">
+                            <table class="table table-striped task-table">
+                                <thead>
+                                <th>Task</th>
+                                <th>&nbsp;</th>
+                                </thead>
+                                <tbody>
+                                @foreach ($tasks as $task)
+                                    <tr>
+                                        <td class="table-text"><div>{{ $task->name }}</div></td>
+
+                                        <!-- Task Delete Button -->
+                                        <td>
+                                            <form action="{{url('task/' . $task->id)}}" method="POST">
+                                                {{ csrf_field() }}
+                                                {{ method_field('DELETE') }}
+
+                                                <button type="submit" id="delete-task-{{ $task->id }}" class="btn btn-danger">
+                                                    <i class="fa fa-btn fa-trash"></i>Delete
+                                                </button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                @endif
             </div>
-
-            <!-- Current Tasks -->
-            @if (count($tasks) > 0)
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        Current Tasks
-                    </div>
-
-                    <div class="panel-body">
-                        <table class="table table-striped task-table">
-                            <thead>
-                            <th>Task</th>
-                            <th>&nbsp;</th>
-                            </thead>
-                            <tbody>
-                            @foreach ($tasks as $task)
-                                <tr>
-                                    <td class="table-text"><div>{{ $task->name }}</div></td>
-
-                                    <!-- Task Delete Button -->
-                                    <td>
-                                        <form action="{{url('task/' . $task->id)}}" method="POST">
-                                            {{ csrf_field() }}
-                                            {{ method_field('DELETE') }}
-
-                                            <button type="submit" id="delete-task-{{ $task->id }}" class="btn btn-danger">
-                                                <i class="fa fa-btn fa-trash"></i>Delete
-                                            </button>
-                                        </form>
-                                    </td>
-                                </tr>
-                            @endforeach
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            @endif
         </div>
-    </div>
 @endsection
